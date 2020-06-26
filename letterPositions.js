bb035dadc5c1c4a0419bde0a889fe76e0773e95b
Oluwatobii <bellotobi10@gmail.com>
2020-06-26 03:35:17 +0000

    A function which will return all the indices (zero-based positions) in the string where each character is found.

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,13 @@
 const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] === arr2[i]) {
-      answer = true;
-    } else {
-      return false;
-    }
+  if (arr1.length !== arr2.length){
+    return false
   }
-  return answer;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
 };
 
 const assertArraysEqual = function(arr1, arr2) {
