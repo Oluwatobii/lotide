a25fa417148a0eb373511e080a3cce210bef7c7e
Oluwatobii <bellotobi10@gmail.com>
2020-06-26 03:30:29 +0000

    A function which will return a subset of a given array, removing unwanted elements.

--- without.js
+++ without.js
@@ -6,16 +6,17 @@ const assertArraysEqual = function (arr1, arr2){
   }
 }
 
-const eqArrays = function (arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++){
-    if (arr1[i] === arr2[i]){
-      answer = true;
-    } else {
-      return false 
-    }
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false
   }
-  return answer
-}
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  }
+  return true;
+};
 
 const without = function (source, itemsToRemove){
   let itemRemaining = [];
@@ -30,10 +31,10 @@ const without = function (source, itemsToRemove){
 // console.log(without([1, 2, 3], [1]))
 // console.log(without(["1", "2", "3"], [1, 2, "3"]))
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+// const words = ["hello", "world", "lighthouse"];
+// without(words, ["lighthouse"]); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
