a234c90f82edda48d67c54e8d65f1a1fc8f30b74
Oluwatobii <bellotobi10@gmail.com>
2020-06-29 17:39:32 +0000

    Refractured my code to include the module.exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual}  === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual}  !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length){
-    return false
+// FUNCTION IMPLEMENTATION
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
-    } 
+    }
   }
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
-assertEqual(eqArrays([3, 2, 3], [1, 2, 4]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "b", "s"], ["1", "b", "B"]), false);
-assertEqual(eqArrays(["1", 2, 3], [1, "2", 4]), false);
+module.exports = eqArrays;
