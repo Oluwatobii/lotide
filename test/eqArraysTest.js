9e1e7bcf9c48d910a1281095f45b0d6989b31e46
Oluwatobii <bellotobi10@gmail.com>
2020-06-29 19:00:43 +0000

    Added the middleTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -12,3 +12,4 @@ assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "b", "s"], ["1", "b", "B"]), false); // => should PASS
 assertEqual(eqArrays(["1", 2, 3], [1, "2", 4]), false); // => should PASS
 assertEqual(eqArrays(["1", 2, 3], [1, 2, 4]), true); // => should FAIL
+assertEqual(eqArrays(["2", "3"], ["2", "3"]), true); // => should FAIL
